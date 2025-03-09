a95757c13a89765cb04a16d0a3c6b45f2afc4e2c
Willy Brauner <willybrauner@gmail.com>
2025-03-09 18:15:46 +0100

    feat: Improve conversion script

--- src/index.ts
+++ src/index.ts
@@ -5,20 +5,18 @@ import chalk from 'chalk'
 import yargs from 'yargs'
 import { hideBin } from 'yargs/helpers'
 import path from 'path'
+
 const argv = yargs(hideBin(process.argv)).argv
 
-const _getVideoHeight = async (
-  inputFile: string,
-  width: number,
-): Promise<number | null> => {
+const _getVideoDimensions = async (inputFile) => {
   try {
     const { stdout } = await execPromise(
-      `ffprobe -v error -select_streams v:0 -show_entries stream=height,width -of csv=s=x:p=0 "${inputFile}"`,
+      `ffprobe -v error -select_streams v:0 -show_entries stream=width,height -of csv=p=0 "${inputFile}"`,
     )
-    const [originalWidth, originalHeight] = stdout.trim().split('x').map(Number)
-    return Math.round((originalHeight / originalWidth) * width)
+    const [originalWidth, originalHeight] = stdout.trim().split(',').map(Number)
+    return { originalWidth, originalHeight }
   } catch (error) {
-    console.error(`Error getting video height: ${error}`)
+    console.error(`Error getting video dimensions: ${error}`)
     return null
   }
 }
@@ -27,16 +25,21 @@ const _getVideoHeight = async (
  * Convert video to a specific width
  */
 const mp4convertor = async ({
-  width = argv.width || argv.w || 1280,
-  crf = argv.crf || 25,
-  bitrate = argv.bitrate || '400K',
+  width = argv.width || argv.w,
+  crf = argv.crf || 23,
+  bitrate = argv.bitrate || '1M',
   inputFile = argv._[0] ? argv._[0] : process.cwd(),
   outputFile = argv.output || argv.o,
 } = {}) => {
-  const height = await _getVideoHeight(inputFile, width)
+  const dimensions = await _getVideoDimensions(inputFile)
+  if (!dimensions) return
+  const { originalWidth, originalHeight } = dimensions
+  if (!width) width = originalWidth
+  const height = Math.round((originalHeight / originalWidth) * width)
 
   console.log('args', {
     width,
+    height,
     crf,
     bitrate,
     inputFile,
@@ -52,7 +55,7 @@ const mp4convertor = async ({
 
   try {
     await execPromise(
-      `ffmpeg -y -i "${inputFile}" -vf "scale=${width}:-2" -c:v libx264 -crf ${crf} -preset ultrafast -b:v ${bitrate} -threads 2 -an "${outputFile}"`,
+      `ffmpeg -y -i "${inputFile}" -vf "scale=${width}:${height}:flags=lanczos" -c:v libx264 -crf ${crf} -preset slow -b:v ${bitrate} -pix_fmt yuv420p -threads 2 -an "${outputFile}"`,
     )
     console.log(chalk.green(`Video has been created: ${outputFile}`))
   } catch (error) {
