e074bfa5f699ceb558e479aaeb930eb06ae2d92e
Willy Brauner <willybrauner@gmail.com>
2025-08-30 11:18:34 +0200

    feart: allow overwrite with -o

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import { promisify } from 'util'
 import { exec } from 'child_process'
-const execPromise = promisify(exec)
+import fs from 'fs'
 import chalk from 'chalk'
 import yargs from 'yargs'
 import { hideBin } from 'yargs/helpers'
 import path from 'path'
 
+const execPromise = promisify(exec)
+const renamePromise = promisify(fs.rename)
 const argv = yargs(hideBin(process.argv)).argv
 
 const _getVideoDimensions = async (inputFile) => {
@@ -46,11 +48,34 @@ const mp4convertor = async ({
 
   const height = _roundToEven((originalHeight / originalWidth) * width)
 
-  if (!outputFile) {
+  const isOverwrite = argv.o || argv.overwrite
+
+  if (!outputFile || typeof outputFile === 'boolean') {
     const filenameWithoutExt = path.basename(inputFile, path.extname(inputFile))
-    const filename = `${filenameWithoutExt}-${width}x${height}.mp4`
+    const filename = isOverwrite
+      ? `${filenameWithoutExt}.mp4`
+      : `${filenameWithoutExt}-${width}x${height}.mp4`
     const outputDir = path.dirname(inputFile)
     outputFile = path.join(outputDir, filename)
+  }
+
+  // If overwrite mode, rename original file with -original suffix
+  if (isOverwrite) {
+    const originalFileExt = path.extname(inputFile)
+    const originalFileWithoutExt = path.basename(inputFile, originalFileExt)
+    const originalFileDir = path.dirname(inputFile)
+    const backupFile = path.join(
+      originalFileDir,
+      `${originalFileWithoutExt}-original${originalFileExt}`,
+    )
+
+    try {
+      await renamePromise(inputFile, backupFile)
+      console.log(`Original file renamed to: ${backupFile}`)
+    } catch (error) {
+      console.error(`Error renaming original file: ${error}`)
+      return
+    }
   }
 
   console.log('args', {
@@ -60,11 +85,19 @@ const mp4convertor = async ({
     bitrate,
     inputFile,
     outputFile,
+    isOverwrite,
   })
 
   try {
+    const sourceFile = isOverwrite
+      ? inputFile.replace(
+          path.basename(inputFile),
+          `${path.basename(inputFile, path.extname(inputFile))}-original${path.extname(inputFile)}`,
+        )
+      : inputFile
+
     await execPromise(
-      `ffmpeg -y -i "${inputFile}" -vf "scale=${width}:${height}:flags=lanczos" -c:v libx264 -crf ${crf} -preset slow -b:v ${bitrate} -pix_fmt yuv420p -threads 2 -an "${outputFile}"`,
+      `ffmpeg -y -i "${sourceFile}" -vf "scale=${width}:${height}:flags=lanczos" -c:v libx264 -crf ${crf} -preset slow -b:v ${bitrate} -pix_fmt yuv420p -threads 2 -an "${outputFile}"`,
     )
     console.log(chalk.green(`Video has been created: ${outputFile}`))
   } catch (error) {
